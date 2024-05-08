5c0b1a29b6b695d1f539f650cc098c0d1e2f809e
At0ned <sethaholt@gmail.com>
2024-05-08 11:22:04 -0500

    Everything is working

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,15 +1,37 @@
+import satData from "./satData";
+
 const Table = ({ sat }) => {
   return (
     <table>
     <thead>
-     <tr>
-       <th>Header TBD</th>
-     </tr>
+      
+        <tr>
+          <th value="name">Name</th>
+          <th value="type">Type of Satellites</th>
+          <th value="launchDate">Launch Date</th>
+          <th value="status">Status</th>
+        </tr>
      </thead>
-     <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
+     <tbody >
+     
+    {sat.map((data) => {
+     return(
+     
+      <tr>
+      <td value="name">{data.name}</td>
+      <td value="type">{data.type}</td>
+      <td value="launchDate">{data.launchDate}</td>
+      {(data.operational) ?
+      <td value="status">active</td> :
+      <td>inactive</td>
+      }
+      </tr>
+      
+     )
+    }
+    )}
+    
+
      </tbody>
    </table>
   );
